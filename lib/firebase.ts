11f400bf92e60c608f861221f03dc8b5d8a2a9d2
Anisha Jindal <anishajindal@Anishas-MacBook-Pro.local>
2025-07-02 12:32:56 +0530

    remove credentials

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,13 +5,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBiwjpCBn40NafqnOwEbsvAGgxQZFsvQGA",
-  authDomain: "yashaweb-d644a.firebaseapp.com",
-  projectId: "yashaweb-d644a",
-  storageBucket: "yashaweb-d644a.appspot.com",
-  messagingSenderId: "705320832063",
-  appId: "1:705320832063:web:fd6cdcebbf1da96f40fc8a",
-  measurementId: "G-7Y705DDTP6"
+  
 };
 
 // Prevent re-initialization in dev mode (Next.js hot reload)
