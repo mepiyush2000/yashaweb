ac3a007cb4efc0e5220998a8eeb2d99af4e23a9e
Piyush Kumar <piyushkuma@adobe.com>
2026-02-08 23:31:26 +0530

    eslint

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@
 const nextConfig = {
   /* config options here */
   images: {
-    domains: ['localhost'],
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'localhost',
+      },
+    ],
   },
 };
 
